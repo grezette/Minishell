94a41a4b5dac022a0563192c8592bfd7f0808af5
grezette <gabrielrezette@hotmail.fr>
2020-09-15 20:28:42 +0200

    Prompt is displaying

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -1,7 +1,25 @@
 #include "../include/minishell.h"
 
+/*
+getcwd: permet d'obtenir le répertoir courant
+chdir : changer le répertoir courant
+*/
+
 int		main(void)
 {
-	ft_printf("Si ca marche je suis un boss et ma bite fait %d cm\n", 25);
+	char *cmd;
+	int	ret;
+
+	while (42)
+	{
+		ft_printf("minishell$ ");
+		if ((ret = get_next_line(0, &cmd)) == -1)
+			exit(0);
+		if (!(ft_strncmp(cmd, "exit", 10)))
+		{
+			free(cmd);
+			exit(0);
+		}
+	}
 	return (0);
 }
